1e20f055a635a07fea2b659c7a83edf826b47792
Dave Earley <dave.m.earley@gmail.com>
2024-06-15 00:04:52 -0700

    Fix email change request going to incorrect email

--- backend/app/Helper/IdHelper.php
+++ backend/app/Helper/IdHelper.php
@@ -11,8 +11,6 @@ class IdHelper
     public const EVENT_PREFIX = 'e';
     public const ACCOUNT_PREFIX = 'acc';
 
-    public const SESSION_COOKIE_PREFIX = 'ord';
-
     public static function randomPrefixedId(string $prefix, int $length = 13): string
     {
         return sprintf('%s%s', $prefix, Str::random($length));
--- frontend/src/utilites/apiClient.ts
+++ frontend/src/utilites/apiClient.ts
@@ -3,13 +3,11 @@ import {publicApi} from "../api/public-client.ts";
 
 export const setAuthToken = (token?: string | undefined | null) => {
     if (!token) {
-        console.log('No token provided');
         delete api.defaults.headers.common['Authorization'];
         delete publicApi.defaults.headers.common['Authorization'];
         return;
     }
 
-    console.log('Setting token:', token);
     api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
     publicApi.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 };
