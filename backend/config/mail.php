a3e97ef8c91baff8145d5b93c87ab80d60b4464e
Dave Earley <dave.m.earley@gmail.com>
2024-06-16 00:14:41 -0700

    Update default mail from name and address

--- backend/config/mail.php
+++ backend/config/mail.php
@@ -98,8 +98,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'hello@hi.events'),
+        'name' => env('MAIL_FROM_NAME', 'Hi.Events'),
     ],
 
     /*
