6d47866e9d5932f63082b8fc089954597d56af81
Dave Earley <dave@hi.events>
2024-09-14 20:18:45 -0700

    Add about & support link

--- frontend/src/components/common/GlobalMenu/index.tsx
+++ frontend/src/components/common/GlobalMenu/index.tsx
@@ -1,19 +1,24 @@
 import {Avatar, Menu, UnstyledButton} from "@mantine/core";
 import {getInitials} from "../../../utilites/helpers.ts";
-import {IconLifebuoy, IconLogout, IconSettingsCog, IconSpeakerphone, IconUser} from "@tabler/icons-react";
+import {IconLifebuoy, IconLogout, IconSettingsCog, IconUser,} from "@tabler/icons-react";
 import {useGetMe} from "../../../queries/useGetMe.ts";
 import {NavLink} from "react-router-dom";
 import {t} from "@lingui/macro";
 import {authClient} from "../../../api/auth.client.ts";
+import {useDisclosure} from "@mantine/hooks";
+import {AboutModal} from "../../modals/AboutModal/index.tsx";
+import {getConfig} from "../../../utilites/config.ts";
 
 export const GlobalMenu = () => {
     const {data: me} = useGetMe();
+    const [aboutModalOpen, {open: openAboutModal, close: closeAboutModal}] =
+        useDisclosure(false);
 
     const links = [
         {
             label: t`My Profile`,
             icon: IconUser,
-            link: '/manage/profile',
+            link: "/manage/profile",
         },
         {
             label: t`Account Settings`,
@@ -21,51 +26,52 @@ export const GlobalMenu = () => {
             link: `/account/settings`,
         },
         {
-            label: t`Help & Support`,
-            icon: IconLifebuoy,
-            link: 'https://hi.events/docs?utm_source=app-top-menu-help-support',
-            target: '_blank',
-        },
-        {
-            label: t`Feedback`,
-            icon: IconSpeakerphone,
-            link: 'mailto:hello@hi.events?subject=Feedback',
-            target: '_blank',
-        },
-        {
             label: t`Logout`,
             icon: IconLogout,
             onClick: (event: any) => {
                 event.preventDefault();
                 authClient.logout();
-                localStorage.removeItem('token');
-                window.location.href = '/auth/login';
-            }
-        }
+                localStorage.removeItem("token");
+                window.location.href = "/auth/login";
+            },
+        },
     ];
 
-    return (
-        <Menu shadow="md" width={200}>
-            <Menu.Target>
-                <UnstyledButton>
-                    <Avatar color={'pink'} radius="xl">
-                        {me
-                            ? getInitials(me.first_name + ' ' + me.last_name)
-                            : '..'}
-                    </Avatar>
-                </UnstyledButton>
-            </Menu.Target>
+    if (!getConfig("VITE_HIDE_ABOUT_LINK")) {
+        links.push({
+            label: `About & Support`,
+            icon: IconLifebuoy,
+            onClick: openAboutModal,
+        });
+    }
 
-            <Menu.Dropdown>
-                {links.map((link) => {
-                    return (
-                        <NavLink onClick={link.onClick} to={link.link ?? '#'} key={link.label}
-                                 target={link.target ?? ''}>
-                            <Menu.Item component={'div'} leftSection={<link.icon/>}>{link.label}</Menu.Item>
+    return (
+        <>
+            <Menu shadow="md" width={200}>
+                <Menu.Target>
+                    <UnstyledButton>
+                        <Avatar color={"pink"} radius="xl">
+                            {me ? getInitials(me.first_name + " " + me.last_name) : ".."}
+                        </Avatar>
+                    </UnstyledButton>
+                </Menu.Target>
+
+                <Menu.Dropdown>
+                    {links.map((link) => (
+                        <NavLink
+                            onClick={link.onClick}
+                            to={link.link ?? "#"}
+                            key={link.label}
+                            target={link.target ?? ""}
+                        >
+                            <Menu.Item component={"div"} leftSection={<link.icon/>}>
+                                {link.label}
+                            </Menu.Item>
                         </NavLink>
-                    );
-                })}
-            </Menu.Dropdown>
-        </Menu>
+                    ))}
+                </Menu.Dropdown>
+            </Menu>
+            {aboutModalOpen && <AboutModal onClose={closeAboutModal}/>}
+        </>
     );
-}
+};
