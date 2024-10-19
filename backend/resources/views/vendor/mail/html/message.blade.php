f2ce92986549f5f9fd915064c548ae21923bd712
Graham Blair <graham@grahamarthurblair.com>
2024-10-19 11:46:18 -0700

    Fix app name variable

--- backend/resources/views/vendor/mail/html/message.blade.php
+++ backend/resources/views/vendor/mail/html/message.blade.php
@@ -37,7 +37,7 @@
                 {{-- In accordance with Section 7(b) of the AGPL, we ask that you retain the "Powered by Hi.Events" notice. --}}
                 {{-- If you wish to remove this notice, a commercial license is available at: https://hi.events/licensing --}}
 
-                © {{ date('Y') }} {{ config('app.app_name') }} | Powered by <a title="Manage events and sell tickets online with Hi.Events" href="https://hi.events?utm_source=app-email-footer">Hi.Events</a>
+                © {{ date('Y') }} {{ config('app.name') }} | Powered by <a title="Manage events and sell tickets online with Hi.Events" href="https://hi.events?utm_source=app-email-footer">Hi.Events</a>
             @endif
         </x-mail::footer>
     </x-slot:footer>
