ce93233e683bea4f716b0b8f8a7629f7549ac3ae
Dave Earley <dave@hi.events>
2024-10-28 21:07:33 -0700

    Fix: Handle deprecated timezones

---
+++ backend/config/timezones.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'deprecated' => [
+        'Asia/Calcutta' => 'Asia/Kolkata',
+        'Asia/Saigon' => 'Asia/Ho_Chi_Minh',
+        'America/Buenos_Aires' => 'America/Argentina/Buenos_Aires',
+        'Europe/Kiev' => 'Europe/Kyiv',
+        'Africa/Asmera' => 'Africa/Asmara',
+        'America/Indianapolis' => 'America/Indiana/Indianapolis',
+        'Asia/Chongqing' => 'Asia/Shanghai',
+        'Asia/Macao' => 'Asia/Macau',
+        'Pacific/Samoa' => 'Pacific/Pago_Pago',
+        'Pacific/Yap' => 'Pacific/Chuuk',
+        'Etc/GMT+0' => 'Etc/GMT',
+        'Etc/Greenwich' => 'Etc/GMT',
+    ],
+];
