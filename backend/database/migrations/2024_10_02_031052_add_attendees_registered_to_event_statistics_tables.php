792f7239cb43e37c60a81c505e5b1c323fc39c5e
Dave Earley <dave@hi.events>
2024-11-03 22:01:47 -0800

    update migration

--- backend/database/migrations/2024_10_02_031052_add_attendees_registered_to_event_statistics_tables.php
+++ backend/database/migrations/2024_10_02_031052_add_attendees_registered_to_event_statistics_tables.php
@@ -7,22 +7,31 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     public function up(): void
     {
-        Schema::table('event_statistics', static function (Blueprint $table) {
-            $table->unsignedInteger('attendees_registered')->default(0);
-        });
-        Schema::table('event_daily_statistics', static function (Blueprint $table) {
-            $table->unsignedInteger('attendees_registered')->default(0);
-        });
+        if (!Schema::hasColumn('event_statistics', 'attendees_registered')) {
+            Schema::table('event_statistics', static function (Blueprint $table) {
+                $table->unsignedInteger('attendees_registered')->default(0);
+            });
+        }
+
+        if (!Schema::hasColumn('event_daily_statistics', 'attendees_registered')) {
+            Schema::table('event_daily_statistics', static function (Blueprint $table) {
+                $table->unsignedInteger('attendees_registered')->default(0);
+            });
+        }
     }
 
     public function down(): void
     {
-        Schema::table('event_statistics_tables', static function (Blueprint $table) {
-            $table->dropColumn('attendees_registered');
-        });
+        if (Schema::hasColumn('event_statistics', 'attendees_registered')) {
+            Schema::table('event_statistics', static function (Blueprint $table) {
+                $table->dropColumn('attendees_registered');
+            });
+        }
 
-        Schema::table('event_daily_statistics', static function (Blueprint $table) {
-            $table->dropColumn('attendees_registered');
-        });
+        if (Schema::hasColumn('event_daily_statistics', 'attendees_registered')) {
+            Schema::table('event_daily_statistics', static function (Blueprint $table) {
+                $table->dropColumn('attendees_registered');
+            });
+        }
     }
 };
