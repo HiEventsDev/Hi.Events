e325817dbb4c66a9e2a9f9fbe0a3e462e87a3aa1
Dave Earley <dave@hi.events>
2024-11-03 22:43:59 -0800

    Update stats migration

--- backend/database/migrations/2024_10_02_031052_add_attendees_registered_to_event_statistics_tables.php
+++ backend/database/migrations/2024_10_02_031052_add_attendees_registered_to_event_statistics_tables.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
@@ -11,12 +12,16 @@ return new class extends Migration {
             Schema::table('event_statistics', static function (Blueprint $table) {
                 $table->unsignedInteger('attendees_registered')->default(0);
             });
+
+            DB::statement('UPDATE event_statistics SET attendees_registered = products_sold');
         }
 
         if (!Schema::hasColumn('event_daily_statistics', 'attendees_registered')) {
             Schema::table('event_daily_statistics', static function (Blueprint $table) {
                 $table->unsignedInteger('attendees_registered')->default(0);
             });
+
+            DB::statement('UPDATE event_daily_statistics SET attendees_registered = products_sold');
         }
     }
 
