87247042af3d6991d09a360adcb699125b9be727
Dave Earley <dave@hi.events>
2025-01-27 08:59:51 +0000

    Add debug logging

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('APP_FRONTEND_URL', '*')],
+    'allowed_origins' => [env('CORS_ALLOWED_ORIGINS', '*')],
 
     'allowed_origins_patterns' => [],
 
