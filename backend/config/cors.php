3d6a710a8fe1c428cd74b32acd36510b5f759f35
Dave Earley <dave@hi.events>
2025-01-31 08:49:40 +0000

    Update axios public client to set withCredentials

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('CORS_ALLOWED_ORIGINS', '*')],
+    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', '*')),
 
     'allowed_origins_patterns' => [],
 
