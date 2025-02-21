f7d5f801adfa3fce87228a743f9d449faa054f95
Dave Earley <dave@hi.events>
2025-02-21 09:22:53 +0000

    Add licence notice

--- frontend/src/components/layouts/AuthLayout/index.tsx
+++ frontend/src/components/layouts/AuthLayout/index.tsx
@@ -28,6 +28,21 @@ const AuthLayout = () => {
                         </div>
                         <div className={classes.wrapper}>
                             <Outlet/>
+                            {
+                                /*
+                                * (c) Hi.Events Ltd 2025
+                                *
+                                * PLEASE NOTE:
+                                *
+                                * Hi.Events is licensed under the GNU Affero General Public License (AGPL) version 3.
+                                *
+                                * You can find the full license text at: https://github.com/HiEventsDev/hi.events/blob/main/LICENCE
+                                *
+                                * In accordance with Section 7(b) of the AGPL, we ask that you retain the "Powered by Hi.Events" notice.
+                                *
+                                * If you wish to remove this notice, a commercial license is available at: https://hi.events/licensing
+                                */
+                            }
                             <PoweredByFooter/>
                         </div>
                     </main>
