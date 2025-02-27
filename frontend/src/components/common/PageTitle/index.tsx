0864af49d9ea43dd1318f279bdcc9270640ce3a9
Dave Earley <dave@hi.events>
2025-02-27 09:44:11 +0000

    Fix incorrect Product and Promo code reports

--- frontend/src/components/common/PageTitle/index.tsx
+++ frontend/src/components/common/PageTitle/index.tsx
@@ -3,12 +3,22 @@ import classes from './PageTitle.module.scss';
 
 interface PageTitleProps extends React.HTMLAttributes<HTMLHeadingElement> {
     children: React.ReactNode,
+    subheading?: string
 }
 
 export const PageTitle = (props: PageTitleProps) => {
+    const { children, subheading, ...rest } = props;
+
     return (
-        <h1 className={classes.title} {...props}>
-            {props.children}
-        </h1>
+        <div className={classes.container}>
+            <h1 className={classes.title} {...rest}>
+                {children}
+            </h1>
+            {subheading && (
+                <div className={classes.subheading}>
+                    {subheading}
+                </div>
+            )}
+        </div>
     );
 }
