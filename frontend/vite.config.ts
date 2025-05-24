90b69f65d1ba019805122ec8c0916213f7f3fd88
creativeindustriesgroup <joe.berry@creativeindustries.group>
2025-05-24 20:28:18 +0100

    fix: use relative paths for assets

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,41 +1,42 @@
-import {defineConfig} from "vite";
-import {lingui} from "@lingui/vite-plugin";
+import { lingui } from "@lingui/vite-plugin";
 import react from "@vitejs/plugin-react";
-import {copy} from "vite-plugin-copy";
+import { defineConfig } from "vite";
+import { copy } from "vite-plugin-copy";
 
 export default defineConfig({
-    optimizeDeps: {
-        include: ["react-router"]
-    },
-    server: {
-        hmr: {
-            port: 24678,
-            protocol: "ws",
-        },
-    },
-    plugins: [
-        react({
-            babel: {
-                plugins: ["macros"],
-            },
-        }),
-        lingui(),
-        copy({
-            targets: [{src: "src/embed/widget.js", dest: "public"}],
-            hook: "writeBundle",
-        }),
-    ],
-    define: {
-        "process.env": process.env,
-    },
-    ssr: {
-        noExternal: ["react-helmet-async"],
-    },
-    css: {
-        preprocessorOptions: {
-            scss: {
-                api: "modern-compiler",
-            }
-        }
-    }
+	base: "./", // Use relative paths for assets
+	optimizeDeps: {
+		include: ["react-router"],
+	},
+	server: {
+		hmr: {
+			port: 24678,
+			protocol: "ws",
+		},
+	},
+	plugins: [
+		react({
+			babel: {
+				plugins: ["macros"],
+			},
+		}),
+		lingui(),
+		copy({
+			targets: [{ src: "src/embed/widget.js", dest: "public" }],
+			hook: "writeBundle",
+		}),
+	],
+	define: {
+		"process.env": process.env,
+	},
+	ssr: {
+		noExternal: ["react-helmet-async"],
+	},
+	css: {
+		preprocessorOptions: {
+			scss: {
+				api: "modern-compiler",
+			},
+		},
+	},
 });
