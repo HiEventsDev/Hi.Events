6eb28f9d68f89b26690631acbfb030299397e94a
creativeindustriesgroup <joe.berry@creativeindustries.group>
2025-06-14 15:43:15 +0100

    fix: ensure fallback is optional

--- frontend/src/utilites/config.ts
+++ frontend/src/utilites/config.ts
@@ -2,7 +2,7 @@ import { ConfigKeys } from "../types.ts";
 import { isSsr } from "./helpers.ts";
 import process from "process";
 
-export const getConfig = ( key: ConfigKeys, fallback: string ): string | undefined => {
+export const getConfig = ( key: ConfigKeys, fallback?: string ): string | undefined => {
     if (isSsr()) {
         const serverEnv = typeof process !== "undefined" && process.env ? process.env : {};
         return serverEnv[ key ] as string | undefined || fallback;
