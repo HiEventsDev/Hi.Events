dad3c076209e4930436ccb91163f2b131ca4ff0f
Carlos Alaniz <carlosglvn93@gmail.com>
2025-06-29 01:18:41 -0500

    refactor(frontend): Replace string-based ConfigKeys with a strict union type and update usages
    - Defined `ConfigKeys` as a union type for better type safety and clarity.
    - Updated `clientBuildEnv` to include all keys from `ConfigKeys`.
    - Enhanced `getConfig` to utilize `clientBuildEnv` for client-side environment variable access.

--- frontend/src/utilites/config.ts
+++ frontend/src/utilites/config.ts
@@ -2,12 +2,32 @@ import { ConfigKeys } from "../types.ts";
 import { isSsr } from "./helpers.ts";
 import process from "process";
 
-export const getConfig = ( key: ConfigKeys, fallback?: string ): string | undefined => {
+
+export const clientBuildEnv: { [K in ConfigKeys]: string } = {
+    'VITE_APP_PRIMARY_COLOR': import.meta.env.VITE_APP_PRIMARY_COLOR,
+    'VITE_APP_SECONDARY_COLOR': import.meta.env.VITE_APP_SECONDARY_COLOR,
+    'VITE_APP_NAME': import.meta.env.VITE_APP_NAME,
+    'VITE_APP_FAVICON': import.meta.env.VITE_APP_FAVICON,
+    'VITE_APP_LOGO_DARK': import.meta.env.VITE_APP_LOGO_DARK,
+    'VITE_APP_LOGO_LIGHT': import.meta.env.VITE_APP_LOGO_LIGHT,
+    'VITE_CHATWOOT_BASE_URL': import.meta.env.VITE_CHATWOOT_BASE_URL,
+    'VITE_CHATWOOT_WEBSITE_TOKEN': import.meta.env.VITE_CHATWOOT_WEBSITE_TOKEN,
+    'VITE_HIDE_ABOUT_LINK': import.meta.env.VITE_HIDE_ABOUT_LINK,
+    'VITE_TOS_URL': import.meta.env.VITE_TOS_URL,
+    'VITE_PRIVACY_URL': import.meta.env.VITVITE_PRIVACY_URL_FRONTEND_URL,
+    'VITE_PLATFORM_SUPPORT_EMAIL': import.meta.env.VITE_PLATFORM_SUPPORT_EMAIL,
+    'VITE_STRIPE_PUBLISHABLE_KEY': import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY,
+    'VITE_I_HAVE_PURCHASED_A_LICENCE': import.meta.env.VITE_I_HAVE_PURCHASED_A_LICENCE,
+    'VITE_FRONTEND_URL': import.meta.env.VITE_FRONTEND_URL,
+    'VITE_DEFAULT_IMAGE_URL': import.meta.env.VITE_DEFAULT_IMAGE_URL,
+}
+
+export const getConfig = (key: ConfigKeys, fallback?: string): string | undefined => {
     if (isSsr()) {
         const serverEnv = typeof process !== "undefined" && process.env ? process.env : {};
-        return serverEnv[ key ] as string | undefined || fallback;
+        return serverEnv[key] as string | undefined || fallback;
     }
 
     const clientEnv = typeof window !== "undefined" && window.hievents ? window.hievents : {};
-    return clientEnv[key] || fallback;
+    return clientEnv[key] || clientBuildEnv[key] || fallback;
 };
