24e8a2f8f6117ef90bf2c88381ffa07cd18390f4
Dave Earley <dave@hi.events>
2025-09-03 23:34:08 -0700

    Fix height of collapses products on event page (#756)

--- frontend/src/components/routes/product-widget/SelectProducts/index.tsx
+++ frontend/src/components/routes/product-widget/SelectProducts/index.tsx
@@ -476,7 +476,7 @@ const SelectProducts = (props: SelectProductsProps) => {
                                                         </UnstyledButton>
                                                     </div>
                                                     <Collapse transitionDuration={100} in={!isProductCollapsed}
-                                                              className={'hi-product-content'}>
+                                                              className={'hi-product-content'} hidden={isProductCollapsed}>
                                                         <div className={'hi-price-tiers-rows'}>
                                                             <TieredPricing
                                                                 productIndex={productIndex++}
