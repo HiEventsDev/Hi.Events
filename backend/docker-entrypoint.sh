dfa7f0681dfef8225634682c4aadbcb52f538fa5
Tijesunimi Peters <tijesunimipeters@gmial.com>
2025-10-20 23:46:12 -0700

    Using entrypoint

---
+++ backend/docker-entrypoint.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+set -eu
+
+echo "[entrypoint] PORT=$PORT"
+
+export SERVER_PORT="${PORT}"
+
+echo "[entrypoint] SERVER_PORT=$SERVER_PORT"
+
+envsubst '$SERVER_PORT' < "/etc/nginx/templates/port.conf.template" > "/etc/nginx/port.conf"
+
+cp /etc/nginx/templates/nginx.conf.template /etc/nginx/nginx.conf
+
+envsubst '$SERVER_PORT' < /etc/nginx/templates/port.conf.template
+
+php-fpm -D
+
+exec nginx -g 'daemon off;'
