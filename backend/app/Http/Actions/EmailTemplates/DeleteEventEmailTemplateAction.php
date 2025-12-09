95feb8f6d48918b7d8eb688ccca564f62861e3f8
Dave Earley <dave@hi.events>
2025-12-09 23:44:16 +0000

    Feature: Disable event templates for unverified users (#948)

--- backend/app/Http/Actions/EmailTemplates/DeleteEventEmailTemplateAction.php
+++ backend/app/Http/Actions/EmailTemplates/DeleteEventEmailTemplateAction.php
@@ -3,15 +3,16 @@
 namespace HiEvents\Http\Actions\EmailTemplates;
 
 use HiEvents\DomainObjects\EventDomainObject;
+use HiEvents\Exceptions\AccountNotVerifiedException;
 use HiEvents\Exceptions\EmailTemplateNotFoundException;
-use HiEvents\Http\Actions\BaseAction;
 use HiEvents\Http\ResponseCodes;
 use HiEvents\Services\Application\Handlers\EmailTemplate\DeleteEmailTemplateHandler;
 use HiEvents\Services\Application\Handlers\EmailTemplate\DTO\DeleteEmailTemplateDTO;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
+use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
 
-class DeleteEventEmailTemplateAction extends BaseAction
+class DeleteEventEmailTemplateAction extends BaseEmailTemplateAction
 {
     public function __construct(
         private readonly DeleteEmailTemplateHandler $handler
@@ -22,6 +23,12 @@ class DeleteEventEmailTemplateAction extends BaseAction
     public function __invoke(int $eventId, int $templateId): Response|JsonResponse
     {
         $this->isActionAuthorized($eventId, EventDomainObject::class);
+
+        try {
+            $this->verifyAccountCanModifyEmailTemplates();
+        } catch (AccountNotVerifiedException $e) {
+            return $this->errorResponse($e->getMessage(), SymfonyResponse::HTTP_UNAUTHORIZED);
+        }
 
         try {
             $this->handler->handle(
