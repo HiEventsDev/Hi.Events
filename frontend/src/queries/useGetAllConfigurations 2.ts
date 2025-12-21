afd94cc06990b475a8e69979bedd92074566e73c
Viktor Kronmar <viktor.kronmar@gmail.com>
2025-12-21 05:28:30 +0100

    remove accidental small files

--- frontend/src/queries/useGetAllConfigurations 2.ts
+++
@@ -1,9 +0,0 @@
-import {useQuery} from "@tanstack/react-query";
-import {adminClient} from "../api/admin.client";
-
-export const useGetAllConfigurations = () => {
-    return useQuery({
-        queryKey: ['admin', 'configurations'],
-        queryFn: () => adminClient.getAllConfigurations(),
-    });
-};
