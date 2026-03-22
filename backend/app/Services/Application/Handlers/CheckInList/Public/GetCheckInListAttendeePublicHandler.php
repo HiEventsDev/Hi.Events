5e52b4e5491e7d524dc1b23391a4589aa7372717
Sundeep Narang <sundeepnrng@gmail.com>
2026-03-22 05:25:32 -0400

    feat: Add password protection and active status validation for public check-in lists, and refactor check-in list retrieval to use a data service.

--- backend/app/Services/Application/Handlers/CheckInList/Public/GetCheckInListAttendeePublicHandler.php
+++ backend/app/Services/Application/Handlers/CheckInList/Public/GetCheckInListAttendeePublicHandler.php
@@ -5,9 +5,10 @@ namespace HiEvents\Services\Application\Handlers\CheckInList\Public;
 use HiEvents\DomainObjects\AttendeeDomainObject;
 use HiEvents\DomainObjects\CheckInListDomainObject;
 use HiEvents\DomainObjects\Generated\CheckInListDomainObjectAbstract;
+use HiEvents\Exceptions\CannotCheckInException;
+use HiEvents\Helper\DateHelper;
 use HiEvents\Repository\Interfaces\AttendeeRepositoryInterface;
 use HiEvents\Services\Domain\CheckInList\CheckInListDataService;
-use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 
 class GetCheckInListAttendeePublicHandler
 {
@@ -21,7 +22,7 @@ class GetCheckInListAttendeePublicHandler
     public function handle(string $shortId, string $attendeePublicId, ?string $password = null): AttendeeDomainObject
     {
         $checkInList = $this->checkInListDataService->getCheckInList($shortId);
-        $this->validateCheckInListIsAuthorized($checkInList, $password);
+        $this->validateCheckInListIsActiveAndAuthorized($checkInList, $password);
 
         return $this->attendeeRepository->findFirstWhere([
             'public_id' => $attendeePublicId,
@@ -29,10 +30,18 @@ class GetCheckInListAttendeePublicHandler
         ]);
     }
 
-    private function validateCheckInListIsAuthorized(CheckInListDomainObject $checkInList, ?string $password): void
+    private function validateCheckInListIsActiveAndAuthorized(CheckInListDomainObject $checkInList, ?string $password): void
     {
         if ($checkInList->isPasswordProtected() && $checkInList->getPassword() !== $password) {
             throw new CannotCheckInException(__('Invalid password provided'));
+        }
+
+        if ($checkInList->getExpiresAt() && DateHelper::utcDateIsPast($checkInList->getExpiresAt())) {
+            throw new CannotCheckInException(__('Check-in list has expired'));
+        }
+
+        if ($checkInList->getActivatesAt() && DateHelper::utcDateIsFuture($checkInList->getActivatesAt())) {
+            throw new CannotCheckInException(__('Check-in list is not active yet'));
         }
     }
 }
